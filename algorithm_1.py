ab4acc5556dc66a5a4dddf061e2f57318805297f
Sarah Greilsamer <sarahgreilsamer@gmail.com>
2017-03-24 00:45:15 -0700

    Add files via upload
    algorithm_1.py - code which we looked at today, to be merged with your code.

---
+++ algorithm_1.py
@@ -0,0 +1,43 @@
+'''Info on data
+Each of the school .mat files has an A matrix (sparse) and a
+"local_info" variable, one row per node: a student/faculty status
+flag, gender, major, second major/minor (if applicable), dorm/house,
+year, and high school. Missing data is coded 0.'''
+
+'''Question: Do we know how many edges each person has?'''
+
+#constants
+FILE_NAME = 'Amherst75'
+
+#read text file
+def prepare_text(filename_data):
+    with open(filename_data) as file:
+        text = file.read()
+    return text
+
+#create lists of students
+def create_students(text):
+    list_students = [] #list of all students where each student is a list
+    lines = text.splitlines() #should return a list of strings
+    for each_line in lines: #for each string in the list of strings append that string
+        list_students.append(each_line.split()) #list of student is a list of lists of strings
+    return list_students
+
+'''def get_regression_coeff(filename_coeff):
+    return list_coeff
+
+def find_attribute(list_students):
+    total_students = 0
+    for each
+    return list_per_attribute
+
+def calculate_weights(list_students):
+    return list_weights
+
+def calculate_score(list_students, list_coeff):
+    return list_influencers'''
+
+text = prepare_text(FILE_NAME)
+list_students = create_students(text)
+print(list_students)
+
