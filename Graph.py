33074555d4a2ae59795c0a76f35901a647ef4b4e
Alirie <gray@oxy.edu>
2017-04-17 10:29:17 -0700

    recursive function works!

--- Graph.py
+++ Graph.py
@@ -12,37 +12,43 @@ def initialize_graph():
     G = nx.Graph()
 
     for friendship in caltech_friendships:
-        G.add_node(int(friendship[0]))
-        G.add_node(int(friendship[1]))
+        G.add_node(int(friendship[0]), hasShared = False, hasSeen = False)
+        G.add_node(int(friendship[1]), hasShared = False, hasSeen = False)
         G.add_edge(int(friendship[0]), int(friendship[1]))
 
     return G
 
-def post_share(G, node, P, seen_so_far):
+def post_share(G, n, P, seen_so_far):
     # give a post to the node
     # P is the likelihood that any given person will share the post
-    # (assume that the first node is guaranteed to share)
-    # if a node "sees" a post, that is, one of their friends has shared the post
+    # if a node "sees" a post, that is, one of their friends has shared the post,
     # then increment the number of people who have seen it
     # and decide if they are also going to share it
 
-    # assume that all of the node's friends see the post
-    seen_so_far += len(G.neighbors(node))
-    list_of_friends = G.neighbors(node)
-    print (list_of_friends)
+    G.node[n]['hasShared'] = True
+    G.node[n]['hasSeen'] = True
 
-    # randomly decide if one of the node's friends will share the post, or if none will
-    r = random.randint(0,1)
-    if (r == 1):
-        seen_so_far += post_share(G, list_of_friends[0], P, seen_so_far)
+    # assume that all of the node's friends see the post
+    # if a node has already seen the post, don't add it to the count of people that have seen it
+    list_of_friends = G.neighbors(n)
+    for student in list_of_friends:
+        if not G.node[student]['hasSeen']:
+            seen_so_far += 1
+            G.node[student]['hasSeen'] = True
+
+    # take roughly P percent of the node's friends
+    select_friends = int (P * len(list_of_friends))
+
+    # randomly pick up to select_friends to share with
+    for i in range (select_friends):
+        r = random.randint(0, len(list_of_friends) - 1)
+        # only share if the friend has not already shared the post before
+        if not G.node[list_of_friends[r]]['hasShared']:
+            G.node[list_of_friends[r]]['hasShared'] = False
+            seen_so_far = post_share(G, list_of_friends[r], P, seen_so_far)
 
     # return the number of people that have seen the post
     return seen_so_far
-
-# we need:
-# the number of friends of the node
-# the array of the node's friends
-
 
 def share_post(node, P):
     pass
